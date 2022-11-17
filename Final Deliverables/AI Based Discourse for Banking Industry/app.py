5127765fda12d939cf8f95e4bd1ceba9f75fae79
Aktha888 <104288128+Aktha888@users.noreply.github.com>
2022-11-17 14:38:47 +0530

    Delete Final Deliverables/AI Based Discourse for Banking Industry directory

--- Final Deliverables/AI Based Discourse for Banking Industry/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask,render_template
-app=Flask(__name__,template_folder='template')
-@app.route('/')
-def Chatbot():
-    return render_template('Chatbot.html')
-if __name__ =='__main__':
-    app.run(debug=True)
